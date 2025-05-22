f696d5e7b7e1c8f8990d127f1063d99e7bab295f
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-05-21 21:36:52 -0300

    iniciando JS
    Criando função de conversões, converter  para RGB e converter para HEXADECIMAL

--- script/script.js
+++ script/script.js
@@ -1,1 +1,45 @@
-console.log("Olá mundo");
+const colorPalette = document.querySelectorAll(".color");
+
+document.querySelector("#colorInput").addEventListener("input", (e) => {
+  changeColor(e);
+});
+
+function toRgb(hex) {
+  hex = hex.replace("#", "");
+  const rgbNum = parseInt(hex, 16);
+
+  return {
+    r: (rgbNum >> 16) & 255,
+    g: (rgbNum >> 8) & 255,
+    b: rgbNum & 255,
+  };
+}
+
+function toHex(r, g, b) {
+  return (
+    "#" +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+      })
+      .join("")
+  );
+}
+
+function changeColor(e) {
+  value = e.target.value;
+
+  let color = [];
+
+  const colorBase = value;
+
+  colorPalette.forEach((card) => {
+    card.style.backgroundColor = value;
+  });
+
+  console.log(value);
+}
+
+// console.log("RGB " + toRgb("#f2f2f2"));
+console.log("HEX " + toHex(242, 242, 242));
