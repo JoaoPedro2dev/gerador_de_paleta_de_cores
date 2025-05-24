f8a9bee53693bf4f68ddcf47bc632ed747dd1571
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-05-23 23:07:04 -0300

    layout
    Refazendo alguns componentes do layout

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+function createImages(array) {
+  const functions = [createFrame, createSpiral];
+
+  functions.forEach((fn) => fn(...array));
+}
+createImages(["#006ed6", "#4092e0", "#80b7eb", "#ccc"]);
+
 function toHex(r, g, b) {
   return (
     "#" +
@@ -22,12 +29,15 @@ function toRgb(hex) {
 }
 
 function createPalette(inputValue) {
+  const cardQnt = document.querySelectorAll(".color").length;
   baseRgb = toRgb(inputValue);
   const white = { r: 255, g: 255, b: 255 };
   let palette = [];
 
-  for (let i = 0; i < 4; i++) {
-    const percent = i / 4;
+  console.log(cardQnt);
+
+  for (let i = 0; i < cardQnt; i++) {
+    const percent = i / cardQnt;
     const r = Math.round(baseRgb.r + (white.r - baseRgb.r) * percent);
     const g = Math.round(baseRgb.g + (white.g - baseRgb.g) * percent);
     const b = Math.round(baseRgb.b + (white.b - baseRgb.b) * percent);
@@ -47,10 +57,7 @@ function addPallet(inputValue) {
     card.querySelector("input").value = colorArray[i];
   });
 
-  createFrame(...colorArray);
-  createWeb(...colorArray);
-  createSpiral(...colorArray);
-  createLandscape(...colorArray);
+  createImages(colorArray);
 }
 
 const linearBox = document.querySelector(".linearBox");
@@ -81,8 +88,6 @@ function copyText(btn) {
   }
 }
 
-//CANVAS JavaScript
-
 function createFrame(c1, c2, c3, c4) {
   const ctx = document.querySelector("#quadro").getContext("2d");
   ctx.clearRect(0, 0, 300, 300);
@@ -98,18 +103,6 @@ function createFrame(c1, c2, c3, c4) {
   console.log(c4);
 }
 
-function createWeb(c1, c2, c3, c4) {
-  const ctx = document.querySelector("#web").getContext("2d");
-  ctx.clearRect(0, 0, 300, 300);
-  ctx.fillStyle = c1;
-  ctx.fillRect(0, 0, 300, 60);
-  ctx.fillStyle = c2;
-  ctx.fillRect(20, 80, 260, 40);
-  ctx.fillStyle = c3;
-  ctx.fillRect(20, 140, 120, 120);
-  ctx.fillRect(160, 140, 120, 120);
-}
-
 function createSpiral(c1, c2, c3, c4) {
   const ctx = document.querySelector("#spiral").getContext("2d");
   const grad = ctx.createRadialGradient(200, 100, 10, 200, 100, 200);
@@ -119,26 +112,6 @@ function createSpiral(c1, c2, c3, c4) {
   ctx.fillStyle = grad;
   ctx.fillRect(0, 0, 400, 200);
 }
-
-function createLandscape(c1, c2, c3, c4) {
-  const ctx = document.querySelector("#landscape").getContext("2d");
-  ctx.clearRect(0, 0, 400, 700);
-  ctx.fillStyle = c3; // céu
-  ctx.fillRect(0, 0, 400, 500);
-  ctx.fillStyle = c2; // Montanha
-  ctx.beginPath();
-  ctx.moveTo(0, 200);
-  ctx.lineTo(100, 80);
-  ctx.lineTo(200, 200);
-  ctx.fill();
-  ctx.fillStyle = c1; //Chão
-  ctx.fillRect(0, 140, 800, 200);
-}
-
-createFrame("#006ed6", "#4092e0", "#80b7eb", "#ccc");
-createWeb("#006ed6", "#4092e0", "#80b7eb", "#ccc");
-createSpiral("#006ed6", "#4092e0", "#80b7eb", "#ccc");
-createLandscape("#006ed6", "#4092e0", "#80b7eb", "#ccc");
 
 const colorInput = document.querySelector("#colorInput");
 colorInput.addEventListener("input", (e) => {
@@ -161,6 +134,10 @@ const inputSecondaryArray = document.querySelectorAll(".inputColorSecondary");
 inputSecondaryArray.forEach((input) => {
   input.addEventListener("input", (e) => {
     input.closest(".color").style.backgroundColor = e.target.value;
+
+    let newArray = Array.from(inputSecondaryArray).map((x) => x.value);
+
+    createImages(newArray);
   });
 });
 
@@ -190,74 +167,3 @@ copyBtn.forEach((btn) => {
     copyText(btn);
   });
 });
-
-//Saturação
-
-const saturationColor = document.querySelector("#saturationInput");
-
-function rgbToHsl(r, g, b) {
-  r /= 255;
-  b /= 255;
-  g /= 255;
-
-  const max = Math.max(r, g, b),
-    min = Math.min(r, g, b);
-
-  let h,
-    s,
-    l = (max, min) / 2;
-
-  if (max == min) {
-    h = s = 0; //Avromatico
-  } else {
-    const d = max - min;
-
-    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < h ? 6 : 0);
-        break;
-      case g:
-        h = (b - r) / d + 2;
-        break;
-      case b:
-        h = (r - g) / d + 4;
-        break;
-    }
-
-    h /= 6;
-  }
-
-  return { h, s, l };
-}
-
-function hslToRgb(h, s, l) {
-  let r, g, b;
-
-  if (s === 0) {
-    r = g = b = l; // Acromático
-  } else {
-    const hue2rgb = (p, q, t) => {
-      if (t < 0) t++;
-      if (t > 1) t--;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * 1 - q;
-
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-
-  return {
-    r: Math.round(r * 255),
-    g: Math.round(g * 255),
-    h: Math.round(b * 255),
-  };
-}
